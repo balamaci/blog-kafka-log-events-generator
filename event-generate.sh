45d31d5d6708f7106807a6e084d45c92850dd107
Serban Balamaci <serban.balamaci@1and1.ro>
2017-03-31 16:10:54 +0300

    update Readme

--- event-generate.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-if [ $# -eq 0 ]
-  then
-    echo "Usage: event-generate.sh kafka_container_name"
-    exit 1
-fi
-
-docker run -it --link $1:kafka --rm --name event_generator -v ~/.m2/:/root/.m2 -v "$PWD":/usr/src/mymaven -w /usr/src/mymaven maven:latest mvn clean compile exec:java
